60811b8c11b51de2d5dc999a2854f629ed3065d4
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2020-02-21 10:07:11 -0700

    backend setup

---
+++ Mountains/src/main/java/com/skilldistillery/mountains/MountainsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mountains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainsApplication.class, args);
+	}
+
+}
---
+++ Mountains/src/test/java/com/skilldistillery/mountains/MountainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mountains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
