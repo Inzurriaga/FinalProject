9198252d1ddbf38e920d2b22616852a92aa0cb87
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2020-02-22 12:20:17 -0700

    create controller, repo, and service for event

--- Mountains/src/main/java/com/skilldistillery/mountains/controllers/EventController.java
+++ Mountains/src/main/java/com/skilldistillery/mountains/controllers/EventController.java
@@ -1,5 +1,25 @@
 package com.skilldistillery.mountains.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.mountains.entities.Event;
+import com.skilldistillery.mountains.services.EventService;
+
+@RestController
+@RequestMapping("api")
 public class EventController {
+	
+	@Autowired
+	private EventService srv;
+	
+	@GetMapping("event/{id}")
+	public Event getEventById(@PathVariable Integer id) {
+		return srv.getEventById(id);
+	}
+	
 
 }
--- Mountains/src/main/java/com/skilldistillery/mountains/services/EventServiceImpl.java
+++ Mountains/src/main/java/com/skilldistillery/mountains/services/EventServiceImpl.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.mountains.services;
 
-public class EventServiceImpl {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.mountains.entities.Event;
+import com.skilldistillery.mountains.repositories.EventRepository;
+
+@Service
+public class EventServiceImpl implements EventService {
+	
+	@Autowired
+	private EventRepository repo;
+
+	@Override
+	public Event getEventById(int id) {
+		Optional<Event> eventOpt = repo.findById(id);
+		if(eventOpt.isPresent()) {
+			return eventOpt.get();
+		}
+		return null;
+	}
 
 }
