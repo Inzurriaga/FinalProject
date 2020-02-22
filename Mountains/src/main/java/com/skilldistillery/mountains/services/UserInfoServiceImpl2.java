69e49e3e0b6a837e7ae632fe1657beb54de438a0
Kai Shu <kaishu83@gmail.com>
2020-02-22 13:37:41 -0700

    nothing

---
+++ Mountains/src/main/java/com/skilldistillery/mountains/services/UserInfoServiceImpl2.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.mountains.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.mountains.entities.UserInfo;
+import com.skilldistillery.mountains.repositories.UserInfoRepository;
+
+public class UserInfoServiceImpl2 implements UserInfoService {
+
+	@Autowired
+	private UserInfoRepository repo;
+
+	@Override
+	public UserInfo findById(Integer id) {
+		Optional<UserInfo> op = repo.findById(id);
+		if(op.isPresent()) {
+			return op.get();
+		}
+		return null;
+	}
+
+}
